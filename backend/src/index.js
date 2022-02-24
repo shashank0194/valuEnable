bd18ab6495995a9c528662006c9d5399010c3613
shashank0194 <shashank0194@gmail.com>
2022-02-24 12:39:45 +0530

    authentication part done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const { signup, signin } = require("./controllers/user.controller")
+
+
+app.post("/signup", signup);
+app.post("/signin", signin);
 module.exports = app
